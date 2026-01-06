59782b54f9bfa833d2963dc588fc32a069ba63ec
Rutuja Nanhe <143776005+rutujananhe@users.noreply.github.com>
2026-01-06 17:51:31 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (94.04%) - LeetSync

--- 1284-four-divisors/four-divisors.cpp
+++ 1284-four-divisors/four-divisors.cpp
@@ -3,25 +3,26 @@ public:
     int sumFourDivisors(vector<int>& nums) {
         int tsum = 0;
         for (auto& it : nums) {
-            int sum = it;
-            int count = 1;
-            for (int i = 1; i <= it / 2; i++) {
+            int sum = 0;
+            int count = 0;
+            for (int i = 1; i * i <= it; i++) {
                 if (it % i == 0) {
-                    sum += i;
-                    count++;
-                }
-                if (count > 4) {
-                    sum = 0;
-                    count = 1;
-                    break;
+                    if (i == it / i) {
+                        count++;
+                        sum += i;
+                    } else {
+                        sum = sum + i + (it / i);
+                        count = count + 2;
+                    }
+                    if (count > 4) {
+                        sum = 0;
+                        break;
+                    }
                 }
             }
-            if (count == 4) {
-                tsum = tsum + sum;
-                count = 1;
-            } else {
-                count = 1;
-            }
+                if (count == 4) {
+                    tsum = tsum + sum;
+                } 
         }
         return tsum;
     }

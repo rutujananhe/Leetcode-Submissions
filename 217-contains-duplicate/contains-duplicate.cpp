292ba0afc4eebfe0c65515be359dc22a9f18659a
Rutuja Nanhe <143776005+rutujananhe@users.noreply.github.com>
2026-01-06 22:09:41 +0530

    Time: 48 ms (61.64%) | Memory: 90.8 MB (44.04%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -3,13 +3,16 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int,int> mp;
         for(auto& i: nums){
+            if(mp.contains(i)){
+                return true;
+            }
             mp[i]++;
         }
-        for(auto& i : mp){
-              if(i.second >= 2){ 
-                return true;
-                }
-          }
+        // for(auto& i : mp){
+        //       if(i.second >= 2){ 
+        //         return true;
+        //         }
+        //   }
           return false;
     }
 };

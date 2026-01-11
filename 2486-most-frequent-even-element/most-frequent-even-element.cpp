b2ac0d4c916d1901baf0bb7a70f1030402be5515
Rutuja Nanhe <143776005+rutujananhe@users.noreply.github.com>
2026-01-11 22:24:45 +0530

    Time: 37 ms (23.43%) | Memory: 42.5 MB (33.46%) - LeetSync

---
+++ 2486-most-frequent-even-element/most-frequent-even-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mostFrequentEven(vector<int>& nums) {
+        map<int,int> mp;
+        for(auto& i:nums){
+            if(i%2==0) mp[i]++;
+        }
+        int max_even=-1;
+        int freq=0;
+        for(auto &[key,val]:mp){
+            if(val>freq){
+                freq=val;
+                max_even=key;
+            }
+        }
+        return max_even;
+    }
+};
